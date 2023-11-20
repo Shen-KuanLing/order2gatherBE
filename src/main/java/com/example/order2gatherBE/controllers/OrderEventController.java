9a2a645cd062cf1f3a27fc5ee4ef1f8d6b18a931
沈冠伶 <sklfad880913@gmail.com>
2023-11-20 17:24:16 +0800

    refactor: move business logic to service

--- src/main/java/com/example/order2gatherBE/controllers/OrderEventController.java
+++ src/main/java/com/example/order2gatherBE/controllers/OrderEventController.java
@@ -3,7 +3,6 @@ package com.example.order2gatherBE.controllers;
 import com.example.order2gatherBE.models.OrderEventModel;
 import com.example.order2gatherBE.services.OrderEventService;
 import com.example.order2gatherBE.exceptions.RequestBodyValidationException;
-import com.example.order2gatherBE.util.SecretCodeGenerator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,34 +15,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.Map;
-
 @RestController
 @RequestMapping(value="/orderEvent", produces = MediaType.APPLICATION_JSON_VALUE)
 @Validated
 public class OrderEventController {
-    private final Logger logger = LoggerFactory.getLogger(getClass());
     @Autowired
     OrderEventService orderEventService;
+
     @PostMapping("/create")
     public ResponseEntity<String> createOrderEvent(@Valid @RequestBody OrderEventModel orderEventModel) {
-        // check whether required data is filled
-        if (orderEventModel.getRId() == null || orderEventModel.getHostID() == null) {
-            logger.error("Invalid OrderEventModel: rid and hostID cannot be null. rid={}, hostID={} is provided.", orderEventModel.getRId(), orderEventModel.getHostID());
-            throw new RequestBodyValidationException("\'rid\' and \'hostID\' are required and the value should be valid.");
-        }
-        // check whether the rid is valid
-
-        // generate 6-digit secret code
-        String secretCode = SecretCodeGenerator.generateSecretCode(orderEventModel.getHostID());
-        orderEventModel.setSecretCode(secretCode);
-
         orderEventService.createOrderEvent(orderEventModel);
 
-        String jsonResponse = String.format("{\"Secret Code\": \"%s\"}", secretCode);
+        String jsonResponse = String.format("{\"Secret Code\": \"%s\"}", orderEventModel.getSecretCode());
         return new ResponseEntity<>(jsonResponse, HttpStatus.OK);
     }
 
