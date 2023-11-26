8093a1fb31e0cb685f809dd5df2e404ba1c456b6
zhi-bao <tomk12171217@gmail.com>
2023-11-27 00:12:33 +0800

    Fixed hello controller for the test.

--- src/main/java/com/example/order2gatherBE/controllers/HelloController.java
+++ src/main/java/com/example/order2gatherBE/controllers/HelloController.java
@@ -1,27 +1,12 @@
 package com.example.order2gatherBE.controllers;
 
-import com.example.order2gatherBE.models.OrderEventModel;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
-
-// import org.json.simple.*;
-import org.json.simple.JSONObject;
-
-import javax.validation.Valid;
-// import org.json.*;
-
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-@RequestMapping(value="/")
 public class HelloController {
-    int counter=0;
-    public ResponseEntity<String> createHello (){
-        return new ResponseEntity<String>("Hello", HttpStatus.OK);
+    @GetMapping(path="/")
+    public String index() {
+        return "Greetings from Spring Boot!";
     }
 }
-
