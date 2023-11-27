0d6f81dba79dc7d6a4d4aa7386e4a8515a487905
fourcolor <james1qaz2wsx12qw@gmail.com>
2023-11-27 23:05:55 +0800

    Make login by passing access token

--- src/main/java/com/example/order2gatherBE/services/AuthenticationService.java
+++ src/main/java/com/example/order2gatherBE/services/AuthenticationService.java
@@ -1,7 +1,17 @@
 package com.example.order2gatherBE.services;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.time.Instant;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.crypto.SecretKey;
 
@@ -10,9 +20,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
-import com.example.order2gatherBE.models.LoginRequest;
 import com.example.order2gatherBE.models.UserModel;
 import com.example.order2gatherBE.repository.AuthenticationRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
@@ -32,17 +44,55 @@ public class AuthenticationService {
 
     private static final long EXPIRE_TIME = 60 * 60 * 1000; // 60 minutes
 
-    public String login(String gmail, String username) {
-        // get uid
-        UserModel user = authenticationRepository.findUserbyGmail(gmail);
-        if (user == null) {
-            authenticationRepository.InsertUser(gmail, username);
-            user = authenticationRepository.findUserbyGmail(gmail);
+    private static Map<String, Object> readJSONtoMap(String jsonString) {
+        if (jsonString == null || jsonString.trim().length() == 0) {
+            return null;
         }
-        return createAccessToken(user.getId());
+
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            TypeReference<HashMap<String, Object>> typeRef = new TypeReference<HashMap<String, Object>>() {
+            };
+            return mapper.readValue(jsonString, typeRef);
+        } catch (JsonProcessingException e) {
+            return null;
+        }
     }
 
-    private String createAccessToken(int uid) {
+    private Map<String, Object> decodeAccessToken(String token) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.googleapis.com/oauth2/v3/userinfo?access_token=" + token))
+                .build();
+        HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
+        return readJSONtoMap(response.body());
+    }
+
+    public String login(String accessToken) {
+
+        try {
+            // decode Access Token
+            Map<String, Object> data;
+            data = decodeAccessToken(accessToken);
+            String gmail = (String) data.get("email");
+            String username = (String) data.get("name");
+
+            // get uid
+            UserModel user = authenticationRepository.findUserbyGmail(gmail);
+            if (user == null) {
+                authenticationRepository.InsertUser(gmail, username);
+                user = authenticationRepository.findUserbyGmail(gmail);
+            }
+            return createAccessToken(user.getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String createAccessToken(int uid) {
         SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes());
         long now = System.currentTimeMillis();
 
