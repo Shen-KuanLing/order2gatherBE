4fef3a2d34480f76c1c8008cf31e469f6c0ae7e1
zhi-bao <tomk12171217@gmail.com>
2023-12-04 16:14:36 +0800

    Fixed import error.

--- src/main/java/com/example/order2gatherBE/controllers/RestaurantController.java
+++ src/main/java/com/example/order2gatherBE/controllers/RestaurantController.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.json.*;
 import java.util.*;
 
 import static com.example.order2gatherBE.util.RestaurantFormatGenerator.*;
